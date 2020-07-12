85f1eb5e79ea03188f94607c4f141fc813a37034
Влад Концевич <kontsevichv@mail.ru>
2020-07-12 11:15:24 +0300

    EX 1.12 (changed)

--- Task_1.12.c
+++ Task_1.12.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 main()
 {
-	int c, state;
-
-	state = OUT;
+	int c;
 
 	while ((c = getchar()) != EOF)
 	{
-		if (c == ' ' || c == '\t')
+		if (c == ' ' || c == '\n' || c == '\t')
 		{
-			state = OUT;
-			printf("\n");
+			putchar('\n');
 		}
-		else if (state == OUT)
-		{
-			state = IN;
-		}
-
-		if (state == IN)
+		else
 		{
 			putchar(c);
 		}
