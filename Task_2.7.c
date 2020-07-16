d5637f52cdf579aba6e7c7394092c21524d90215
Влад Концевич <kontsevichv@mail.ru>
2020-07-16 17:20:13 +0300

    EX 2.7

---
+++ Task_2.7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+main()
+{
+	printf("%d", invert(255, 2, 1));
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+	return x ^ (~(~0 << n) << p);
+}
