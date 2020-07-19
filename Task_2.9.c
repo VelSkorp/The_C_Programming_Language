f98992bb475c5f803994932fd5f7d433356a916a
Влад Концевич <kontsevichv@mail.ru>
2020-07-19 12:11:07 +0300

    EX 2.9

---
+++ Task_2.9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int bitcount(unsigned x);
+
+main()
+{
+	printf("%d\n", bitcount(50));
+
+	return 0;
+}
+
+int bitcount(unsigned x)
+{
+	int b;
+
+	for (b = 0; x != 0; x &= (x - 1))
+	{
+		b++;
+	}
+
+	return b;
+}
