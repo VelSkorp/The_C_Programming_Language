ed64ef3579dfc358d3c5dc4dd86f45603ab81e78
Влад Концевич <kontsevichv@mail.ru>
2020-08-13 12:53:23 +0300

    EX 7.1

---
+++ Task_7.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+main(int argc, char* argv[])
+{
+	int c;
+
+	if (strstr(argv[0], "lower") == 0)
+	{
+		while ((c = getchar()) != EOF)
+		{
+			putchar(tolower(c));
+		}
+	}
+	else
+	{
+		while ((c = getchar()) != EOF)
+		{
+			putchar(toupper(c));
+		}
+	}
+
+	return 0;
+}
